8b3da28f3c3d6a8a55d6a271a5a87b28e89195b8
Justin Grant <jlgrock@hotmail.com>
2013-11-27 11:35:33 -0500

    updating java files to remove compiler warnings and raw generics (where possible)

--- src/main/java/com/altamiracorp/bigtable/model/Repository.java
+++ src/main/java/com/altamiracorp/bigtable/model/Repository.java
@@ -7,21 +7,21 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Map;
 
-public abstract class Repository<T extends Row> {
+public abstract class Repository<T extends Row<? extends RowKey>> {
     private ModelSession modelSession;
 
     public Repository(ModelSession modelSession) {
         this.modelSession = modelSession;
     }
 
-    public abstract T fromRow(Row row);
+    public abstract T fromRow(Row<? extends RowKey> row);
 
-    public abstract Row toRow(T obj);
+    public abstract Row<? extends RowKey> toRow(T obj);
 
     public abstract String getTableName();
 
     public T findByRowKey(String rowKey, ModelUserContext user) {
-        Row row = modelSession.findByRowKey(getTableName(), rowKey, user);
+        Row<? extends RowKey> row = modelSession.findByRowKey(getTableName(), rowKey, user);
         if (row == null) {
             return null;
         }
@@ -31,7 +31,7 @@ public abstract class Repository<T extends Row> {
     }
 
     public T findByRowKey(String rowKey, Map<String, String> columnsToReturn, ModelUserContext user) {
-        Row row = modelSession.findByRowKey(getTableName(), rowKey, columnsToReturn, user);
+        Row<? extends RowKey> row = modelSession.findByRowKey(getTableName(), rowKey, columnsToReturn, user);
         if (row == null) {
             return null;
         }
@@ -41,25 +41,25 @@ public abstract class Repository<T extends Row> {
     }
 
     public List<T> findByRowStartsWith(String rowKeyPrefix, ModelUserContext user) {
-        Collection<Row> rows = modelSession.findByRowStartsWith(getTableName(), rowKeyPrefix, user);
+        Collection<Row<? extends RowKey>> rows = modelSession.findByRowStartsWith(getTableName(), rowKeyPrefix, user);
         return fromRows(rows);
     }
 
     public List<T> findAll(ModelUserContext user) {
-        Collection<Row> rows = modelSession.findByRowStartsWith(getTableName(), null, user);
+        Collection<Row<? extends RowKey>> rows = modelSession.findByRowStartsWith(getTableName(), null, user);
         return fromRows(rows);
     }
 
     public void save(T obj, ModelUserContext user) {
-        Row r = toRow(obj);
+        Row<? extends RowKey> r = toRow(obj);
         modelSession.save(r, user);
     }
 
     public void saveMany(Collection<T> objs, ModelUserContext user) {
-        List<Row> rows = new ArrayList<Row>();
+        List<Row<? extends RowKey>> rows = new ArrayList<Row<? extends RowKey>>();
         String tableName = null;
         for (T obj : objs) {
-            Row row = toRow(obj);
+            Row<? extends RowKey> row = toRow(obj);
             if (tableName == null) {
                 tableName = row.getTableName();
             }
@@ -68,9 +68,9 @@ public abstract class Repository<T extends Row> {
         modelSession.saveMany(tableName, rows, user);
     }
 
-    public List<T> fromRows(Collection<Row> rows) {
+    public List<T> fromRows(Collection<Row<? extends RowKey>> rows) {
         ArrayList<T> results = new ArrayList<T>();
-        for (Row row : rows) {
+        for (Row<? extends RowKey> row : rows) {
             T r = fromRow(row);
             r.setDirtyBits(false);
             results.add(r);
--- src/main/java/com/altamiracorp/bigtable/model/RowKeyComparator.java
+++ src/main/java/com/altamiracorp/bigtable/model/RowKeyComparator.java
@@ -2,9 +2,9 @@ package com.altamiracorp.bigtable.model;
 
 import java.util.Comparator;
 
-public class RowKeyComparator implements Comparator<Row> {
+public class RowKeyComparator implements Comparator<Row<? extends RowKey>> {
     @Override
-    public int compare(Row row1, Row row2) {
+    public int compare(Row<? extends RowKey> row1, Row<? extends RowKey> row2) {
         return row1.getRowKey().toString().compareTo(row2.getRowKey().toString());
     }
 }
