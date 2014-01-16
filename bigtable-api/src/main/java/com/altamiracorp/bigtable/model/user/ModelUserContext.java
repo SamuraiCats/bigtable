745fcbfa82b7c3ada91e41a111d0b86522c4f94e
Joe Ferner <joe@fernsroth.com>
2014-01-16 16:58:38 -0500

    ModelUserContext should be Serializable

--- bigtable-api/src/main/java/com/altamiracorp/bigtable/model/user/ModelUserContext.java
+++ bigtable-api/src/main/java/com/altamiracorp/bigtable/model/user/ModelUserContext.java
@@ -1,7 +1,9 @@
 package com.altamiracorp.bigtable.model.user;
 
+import java.io.Serializable;
+
 /**
  * Empty interface, to be used by implementations to hold pertinent information about the user currently logged in to an application
  */
-public interface ModelUserContext {
+public interface ModelUserContext extends Serializable {
 }
